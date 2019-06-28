be49f2a2619a8daaada363fa030b75ec7f567642
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-28 23:06:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,5 @@
 def roll_call_dwarves(names)
-  new_array = []
-  i = 0
-  num = 1
-  while i < names.length do
-    names.each do |dwarf|
-      new_array << num + dwarf[i]
-      i + 1 
-      num + 1
-    end
-  end
-  puts new_array
+  names.each_with_index
 end
 
 def summon_captain_planet# code an argument here
