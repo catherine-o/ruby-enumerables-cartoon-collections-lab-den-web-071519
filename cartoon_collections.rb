517e87a10d99f97b03ca8bbe4ba6fabb35226021
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-29 00:39:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.any?{|cheese| 
+  if array.any?{|cheese| cheese_types.include?(cheese)}
     return 
 
 end
