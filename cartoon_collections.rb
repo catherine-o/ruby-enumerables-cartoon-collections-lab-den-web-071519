d8d4bcf8e3cba2f202070a590fdfadc5107d6223
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-29 00:49:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array)
   new array = []
   if array.any?{|cheese| cheese_types.include?(cheese)}
     new_array << array & cheese_types
-    return new array[0]
+    return new_array[0]
   else
     return
   end
